fb1bcfe3e2913860647e9980b76b4d602a112967
zarezza <nfahreza01@gmail.com>
2024-10-10 20:10:15 +0700

    Add new pages

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+
+Route::get('/invoices', function () {
+    return view('invoices');
 });
